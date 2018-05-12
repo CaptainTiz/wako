3365b1f7d78d416cd005c5aee34b78d7ea041d34
Tiz <tiz@wako.cpe.cableonda.net>
2018-05-12 15:29:56 -0500

    porting heart from pure js to expressjs

--- server.js
+++ server.js
@@ -1,38 +1,9 @@
 
-
 //Settings
-var http = require('http');
-var url = require('url');
-var querystring = require('querystring');
+
+const express = require('express');
+const app = express();
 
 
-//Body
-var server = http.createServer( function(req, res){
-    var params = querystring.parse(url.parse(req.url).query);
-    var page = url.parse(req.url).pathname;    
-    console.log(page);
-    console.log(params);
-    res.writeHead(200, { "Content-type":"text/plain" } );
-    
-    if( page == '/'){
-        res.write('velcome papi');
-    }
-    else if ( page == '/sexe' ){
-        res.write('Well Hello Papi');
-
-        if('firstname' in params && 'lastname' in params){
-            res.write('Papi, your name is' + params['firstname'] + ' ' + params['lastname']);
-        }
-        else{
-            res.write('who the f are you?');
-        }
-    }
-    else if ( page == '/test' ){
-        res.write('Testing the test papi');
-    }
-
-    res.end('Beating Ferverntly');
-
-});
-
-server.listen(8080);
+app.get('/', (req, res) => res.send('Beating though Expressjs'));
+app.listen(3000, () => console.log('Example heartbeat on port 3000'));
